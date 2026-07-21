265991651ae72b14ee49e3a21c1771de1d9abaf2
jerukelektrik <jerukelektrik@gmail.com>
2026-07-21 18:26:04 +0700

    Fix: Add missing ss-review-carousel class to enable touch drag and disable snap lock during drag

--- front-page.php
+++ front-page.php
@@ -1085,7 +1085,7 @@ $post_count = $reviews->post_count;
 			<!-- Carousel/Slider Layout (100% Fluid Touch Slideable on Mobile) -->
 			<div class="relative w-full group" id="poetzen-review-carousel-wrapper">
 				<!-- Scroll Container -->
-				<div id="poetzen-review-scroll" class="-mx-4 flex gap-4 overflow-x-auto snap-x snap-mandatory no-scrollbar px-4 pb-2 sm:-mx-6 sm:px-6 md:mx-0 md:px-0 md:gap-5 items-stretch" data-drag-scroll>
+				<div id="poetzen-review-scroll" class="ss-review-carousel -mx-4 flex gap-4 overflow-x-auto snap-x snap-mandatory no-scrollbar px-4 pb-2 sm:-mx-6 sm:px-6 md:mx-0 md:px-0 md:gap-5 items-stretch" data-drag-scroll>
 					<?php while ( $reviews->have_posts() ) : $reviews->the_post(); ?>
 						<div class="w-[72vw] max-w-[17rem] shrink-0 snap-start sm:w-[42vw] md:w-[calc((100%-3*1.25rem)/4)] lg:w-[calc((100%-3*1.25rem)/4)] flex">
 							<?php get_template_part( 'template-parts/cards/review-card', null, array( 'layout' => 'vertical' ) ); ?>
